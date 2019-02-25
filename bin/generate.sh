3345b223eb7676e386daa45e351a10909cec4344
Ralf Hartung <ralf.hartung@contorion.de>
2019-02-25 23:41:59 +0100

    ZDB directory moved to variable

--- bin/generate.sh
+++ bin/generate.sh
@@ -5,25 +5,27 @@ if [[ "$1" == "" ]]; then
     exit
 fi
 
+zdb_location="/home/rhartung/cuthbert/klingon-data/data/dict-gen.zdb"
+
 make distclean
 make clean
 
 lang_code="";
 if [[ "$1" == "english" || "$1" == "all" ]]; then
-    ./bin/zdb2tex run tlh en ~/cuthbert/klingon-data/data/dict-gen.zdb > tex/en/include/tlh-en.tex
-    ./bin/zdb2tex run en tlh ~/cuthbert/klingon-data/data/dict-gen.zdb > tex/en/include/en-tlh.tex
+    ./bin/zdb2tex run tlh en $zdb_location > tex/en/include/tlh-en.tex
+    ./bin/zdb2tex run en tlh $zdb_location > tex/en/include/en-tlh.tex
     make english
 fi
 
 if [[ "$1" == "german" || "$1" == "all" ]]; then
-    ./bin/zdb2tex run tlh de ~/cuthbert/klingon-data/data/dict-gen.zdb > tex/de/include/tlh-de.tex
-    ./bin/zdb2tex run de tlh ~/cuthbert/klingon-data/data/dict-gen.zdb > tex/de/include/de-tlh.tex
+    ./bin/zdb2tex run tlh de $zdb_location > tex/de/include/tlh-de.tex
+    ./bin/zdb2tex run de tlh $zdb_location > tex/de/include/de-tlh.tex
     make german
 fi
 
 if [[ "$1" == "swedish" || "$1" == "all" ]]; then
-    ./bin/zdb2tex run tlh sv ~/cuthbert/klingon-data/data/dict-gen.zdb > tex/sv/include/tlh-sv.tex
-    ./bin/zdb2tex run sv tlh ~/cuthbert/klingon-data/data/dict-gen.zdb > tex/sv/include/sv-tlh.tex
+    ./bin/zdb2tex run tlh sv $zdb_location > tex/sv/include/tlh-sv.tex
+    ./bin/zdb2tex run sv tlh $zdb_location > tex/sv/include/sv-tlh.tex
     make swedish
 fi
 
